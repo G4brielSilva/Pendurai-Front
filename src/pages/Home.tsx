c36958babe16fa20b775a29c1ed32cb6dd2f03bd
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2025-05-23 20:53:43 -0300

    feat: adicionar página home e sidebar ao projeto

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import type React from 'react';
+import './Home.css';
+import Sidebar from '../components/Sidebar';
+
+const Home: React.FC = () => {
+  return (
+    <>
+		<Sidebar/>
+    </>
+  );
+};
+
+export default Home;
