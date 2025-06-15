b29024f97053afc4a2477985ce6175cd0b6f8fb1
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2025-06-15 15:59:53 -0300

    feat: integrando layout com telas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Clients from './pages/Clients';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
@@ -9,7 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
-		<Route path="/home" element={<Home />} />
+        <Route element={<Layout />}>
+			<Route path="/home" element={<Home />} />\
+			<Route path="/clients" element={<Clients />} />
+        </Route>
       </Routes>
     </Router>
   );
